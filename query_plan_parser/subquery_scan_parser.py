14cb0e4b0d688f46204588f507fa010130b41c95
kenfrand <kenfrand@gmail.com>
2017-11-15 17:17:50 +0800

    added unit test

--- query_plan_parser/subquery_scan_parser.py
+++ query_plan_parser/subquery_scan_parser.py
@@ -15,3 +15,20 @@ def subquery_scan_parser(sentence):
     result += ' the Subquery Scan process the result from the previous operations and output it without any changes. The purpose of Subquery scan is mainly for internal bookkeeping.'
       
     return result
+
+if __name__ == "__main__":
+    test = '''
+    {                                                   
+        "Node Type": "Subquery Scan",                                     
+        "Parent Relationship": "Outer",                                   
+        "Parallel Aware": false,                                          
+        "Alias": "tmp_a",                                                 
+        "Startup Cost": 48103.23,                                         
+        "Total Cost": 48113.40,                                           
+        "Plan Rows": 170,                                                 
+        "Plan Width": 15,                                                 
+        "Filter": "(NOT (hashed SubPlan 1))"
+    }
+    '''
+    test_plan = json.loads(test)
+    print(subquery_scan_parser(test_plan))
